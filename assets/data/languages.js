d2e6d60e55996efacfacf1fa6764de9f6188b419
chabanyukm <chabanyuk.maks@gmail.com>
2023-01-30 13:34:24 +0200

    ability to change language

---
+++ assets/data/languages.js
@@ -0,0 +1,14 @@
+export default {
+    "en": {
+        "draw-your-graph-text": "Build your graph",
+        "choose-language-text": "Choose language of the interface",
+        "weighted-label-text": ["Unweighted", "Weighted"],
+        "directed-label-text": ["Undirected", "Directed"],
+    },
+    "ua": {
+        "draw-your-graph-text": "Побудуй свій граф",
+        "choose-language-text": "Обери мову інтерфесу",
+        "weighted-label-text": ["Незважений", "Зважений"],
+        "directed-label-text": ["Неорієнтований", "Орієнтований"],
+    }
+}
