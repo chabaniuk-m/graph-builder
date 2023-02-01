df0781842ee59b6fd3d31fec595308fc090cf659
chabanyukm <chabanyuk.maks@gmail.com>
2023-02-01 15:07:20 +0200

    weight input

--- index.js
+++ index.js
@@ -12,6 +12,23 @@ console.log(`Max negative integer len: ${('' + Number.MIN_SAFE_INTEGER).length},
 console.log(`Max positive number len: ${('' + Number.MAX_VALUE).length}, the number is ${Number.MAX_VALUE}`)
 console.log(`Max negative number len: ${('' + Number.MIN_VALUE).length}, the number is ${Number.MIN_VALUE}`)
 
+function download(filename, text) {
+  let element = document.createElement("a")
+  element.style.display = "none"
+  element.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(text))
+  element.setAttribute("download", filename)
+  document.body.appendChild(element)
+  element.click()
+  document.body.removeChild(element)
+}
+
+document.getElementById("download-btn").addEventListener("click", (event) => {
+  let text = document.getElementById("text-val").value
+  let filename = document.getElementById("filename").value
+
+  download(filename, text)
+}, false)
+
 // UTILS
 const popupEl = document.getElementById("popup")
 
@@ -26,27 +43,96 @@ export function popupMessage(message, time=3000) {
   }, time)
 }
 
-export function isDigit(char) {
-  return ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(char)
+export function hidePopup() {
+  popupEl.classList.remove("alert")
+  clearTimeout(popupTimer)
 }
 
-// TEST
-const numberEl = document.getElementById("number")
-numberEl.addEventListener("keydown", (event) => {
-  const key = event.key
+export function isDigit(str) {
+  for (let i = 0; i < str.length; i++)
+    if (!['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(str.charAt(i)))
+      return false
+  return str.length > 0
+}
+
+function numberInputHandler(event){
+  let key = event.key
   const val = event.target.value
+  console.log(`
+  Processing keydown event:
+  1) Type = ${event.type},
+  2) Value = ${val},
+  3) Key = ${key}
+  `)
   if (key === " ") {
     if (parseInt(event.target.cols, 10) + 2 === val.length)
       event.target.cols = parseInt(event.target.cols, 10) + 1
     return
-  } else if (["Tab", "r", "Alt", "Capslock", "Meta", "Shift", "Control", "Delete", "PageUp", "ArrowRight", "End", "PageDown"].includes(key))
+  } else if (["r", "Alt", "Capslock", "Meta", "Shift", "Control", "Delete", "PageUp", "ArrowRight", "End", "PageDown"].includes(key))
     return
-  event.preventDefault()
   let save = true
   let removeWarning = true
   let valWithoutSpaces = val.replaceAll(" ", "")
+  if (key === "Tab") {
+    return
+  }
+  event.preventDefault()
   console.log(`current value is ${val}, possible updated: ${val + key} = ${parseFloat(valWithoutSpaces + key)}`)
   let length = valWithoutSpaces.length
+  const checkIntegerInput = () => {
+    console.log(`Checking the integer ${valWithoutSpaces} of length ${length}, additional key = '${key}'`)
+    if (valWithoutSpaces[0] === '-') {
+      console.log("Negative")
+      // negative integer
+      if (length === 17) {
+        if (key !== '') {
+          save = false
+          popupMessage(langPack["negative-int-restriction-text"])
+        } else {
+          console.log("check if number is not too big")
+          let number = parseInt(valWithoutSpaces + key)
+          if (number < Number.MIN_SAFE_INTEGER) {
+            popupMessage(langPack["number-underflow-text"].replace("${}", "-9 007 199 254 740 991"))
+            event.target.classList.add("warning")
+            removeWarning = false
+          }
+        }
+      } else if (length === 16 && key !== '') {
+        console.log("check if number is not too big")
+        let number = parseInt(valWithoutSpaces + key)
+        if (number < Number.MIN_SAFE_INTEGER) {
+          popupMessage(langPack["number-underflow-text"].replace("${}", "-9 007 199 254 740 991"))
+          event.target.classList.add("warning")
+          removeWarning = false
+        }
+      }
+    } else {
+      console.log("Positive")
+      // positive integer
+      if (length === 15 && key !== '') {
+        console.log("check if number is not too big")
+        let number = parseInt(valWithoutSpaces + key)
+        if (number > Number.MAX_SAFE_INTEGER) {
+          popupMessage(langPack["number-overflow-text"].replace("${}", "9 007 199 254 740 991"))
+          event.target.classList.add("warning")
+          removeWarning = false
+        }
+      } else {
+        if (key !== '') {
+          save = false
+          popupMessage(langPack["positive-int-restriction-text"])
+        } else {
+          console.log("check if number is not too big")
+          let number = parseInt(valWithoutSpaces + key)
+          if (number > Number.MAX_SAFE_INTEGER) {
+            popupMessage(langPack["number-overflow-text"].replace("${}", "9 007 199 254 740 991"))
+            event.target.classList.add("warning")
+            removeWarning = false
+          }
+        }
+      }
+    }
+  }
   if (['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '+', '.', 'e'].includes(key)) {
     if (key === '.' && (valWithoutSpaces.includes('.') || valWithoutSpaces === '-' || length === 0) ||
         key === 'e' && (valWithoutSpaces.includes('e') || length === 0 || isNaN(valWithoutSpaces.charAt(length - 1))) ||
@@ -76,14 +162,18 @@ numberEl.addEventListener("keydown", (event) => {
       number = parseFloat(valWithoutSpaces + key)
       let a = Math.abs(number)
       console.log(`value with key = ${number}`)
-      if (a > Number.MAX_VALUE ||
-          a < Number.MIN_VALUE ||
-          a === 0) {
-        if (a !== 0) {
-          event.target.classList.add("warning")
-          removeWarning = false
+      if (length !== 24) {
+        if (a > Number.MAX_VALUE ||
+            a < Number.MIN_VALUE ||
+            a === 0) {
+          if ((valWithoutSpaces + key).includes('e')) {
+            event.target.classList.add("warning")
+            removeWarning = false
+          }
+          popupMessage(langPack["number-out-of-bound-text"].replace("${}", `${number}`))
         }
-        popupMessage(langPack["number-out-of-bound-text"].replace("${}", `${number}`))
+      } else {
+        save = false
       }
     } else if (length > 14) {
       if (valWithoutSpaces.includes('.') || key === '.') {
@@ -92,35 +182,8 @@ numberEl.addEventListener("keydown", (event) => {
           popupMessage(langPack["number-input-length-restriction-text"])
           save = false
         }
-      } else if (valWithoutSpaces[0] === '-') {
-        // negative integer
-        if (length === 17) {
-          save = false
-          popupMessage(langPack["negative-int-restriction-text"])
-        } else if (length === 16) {
-          console.log("check if number is not too big")
-          let number = parseInt(valWithoutSpaces + key)
-          if (number < Number.MIN_SAFE_INTEGER) {
-            popupMessage(langPack["number-underflow-text"].replace("${}", "-9 007 199 254 740 991"))
-            event.target.classList.add("warning")
-            removeWarning = false
-          }
-        }
-      } else {
-        // positive integer
-        if (length === 15) {
-          console.log("check if number is not too big")
-          let number = parseInt(valWithoutSpaces + key)
-          if (number > Number.MAX_SAFE_INTEGER) {
-            popupMessage(langPack["number-overflow-text"].replace("${}", "9 007 199 254 740 991"))
-            event.target.classList.add("warning")
-            removeWarning = false
-          }
-        } else {
-          save = false
-          popupMessage(langPack["positive-int-restriction-text"])
-        }
-      }
+      } else
+        checkIntegerInput()
 
     }
     if (save) {
@@ -130,32 +193,47 @@ numberEl.addEventListener("keydown", (event) => {
       event.target.value += key
       if (isNaN(parseFloat(event.target.value)))
         event.target.classList.add("warning")
-      else if(removeWarning && event.target.classList.contains("warning"))
+      else if(removeWarning && event.target.classList.contains("warning")) {
         event.target.classList.remove("warning")
+        hidePopup()
+      }
     }
   } else if (key === "Backspace") {
     event.target.value = val ? val.substring(0, val.length - 1) : ""
+    key = ''
     if (parseInt(event.target.cols, 10) > 2)
       event.target.cols = parseInt(event.target.cols, 10) - 1
-    if (event.target.classList.contains("warning"))
-      event.target.classList.remove("warning")
+    valWithoutSpaces = event.target.value.replaceAll(" ", "")
+    length = valWithoutSpaces.length
+    if (valWithoutSpaces.length > 14 &&
+        isDigit(valWithoutSpaces.charAt(0) === '-' ?
+            valWithoutSpaces.substring(1) : valWithoutSpaces)) {
+      console.log("Checking for overflow")
+      checkIntegerInput()
+    }
+    if (removeWarning) {
+      hidePopup()
+      if (event.target.classList.contains("warning")) {
+        event.target.classList.remove("warning")
+      }
+    }
     if (isNaN(parseFloat(event.target.value)) || length < 2)
       if (!event.target.classList.contains("warning"))
         event.target.classList.add("warning")
   } else if (key === "Enter") {
-    if (!val.includes(".") && !val.includes("e")) {
-      let weight = parseInt(val, 10)
-      weight = weight > Number.MAX_SAFE_INTEGER ? Number.MAX_SAFE_INTEGER : weight;
-      console.log(`User have entered an integer ${weight}`)
-    }
-    // TODO: remove focus and save the weight
+    event.target.blur()
+    saveWeight()
   } else if (["ArrowLeft", "Home"].includes(key)) {
     popupMessage(langPack["cursor-moving-is-forbidden-text"], 2000)
   } else {
     popupMessage(langPack["enter-correct-number-text"], 1500)
     console.log(`Incorrect symbol in number input: ${key}`)
   }
-})
+}
+
+function saveWeight(useInput, edgeID) {
+  console.log("Saving the weight...")
+}
 
 
 let language = "ua",
@@ -165,6 +243,7 @@ let language = "ua",
   isWeighted = false,
   graph = {
     directed: false,
+    weighted: false,
     nodes: [],
   },
   activeNodeID = -1,
@@ -197,7 +276,7 @@ languageEl.addEventListener("change", (event) => {
 function disableProperties() {
   weightBtnEl.disabled = "true"
   directedBtnEl.disabled = "true"
-  propsEl.style.opacity = "0.6";
+  propsEl.style.opacity = "0.6"
 }
 
 function enableProperties() {
@@ -305,6 +384,7 @@ function drawNode(x, y) {
 function drawEdge(x1, y1, x2, y2, id1, id2) {
   log(`Adding edge (${x1}, ${y1}) ${isDirected ? '' : '<'}-> (${x2}, ${y2})`)
   const lineEl = document.createElement("div")
+  lineEl.id = `${id1}-${id2}`
   const length = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2))
   lineEl.style.width = `${length}px`
   lineEl.style.left = `${(x1 + x2) / 2.0}px`
@@ -313,20 +393,10 @@ function drawEdge(x1, y1, x2, y2, id1, id2) {
   const v2 = [x2 - x1, y2 - y1]
   const angel = Math.acos((v2[0]) / Math.sqrt(Math.pow(v2[0], 2) + Math.pow(v2[1], 2)))
   lineEl.style.transform = `translate(-50%) rotate(${y2 > y1 ? angel : -angel}rad)`
-  
-  // add an arrow
-  if (isDirected) {
-    const arrowEl = document.createElement("span")
-    arrowEl.classList.add("arrow")
-    arrowEl.innerText = '>'
-    arrowEl.style.right = `${nodeRadius}px`
-    arrowEl.style.top = `-${arrowHalfHeight}px`
-    // arrowEl.style.transform = `rotate(${y2 > y1 ? angel : -angel}rad)`
-    lineEl.appendChild(arrowEl)
-  }
-  lineEl.addEventListener("dblclick", (event) => {
-    const id = event.target.id
-    boardEl.removeChild(event.target)
+
+  const removeLine = (target) => {
+    const id = target.id
+    boardEl.removeChild(target)
     const [from, to] = id.split('-')
     let node = graph.nodes.find(node => node.id == from)
     log(`${from} -> ${to} (${JSON.stringify(node)})`)
@@ -336,9 +406,39 @@ function drawEdge(x1, y1, x2, y2, id1, id2) {
       node.pointsTo = node.pointsTo.filter(x => x.id != from)
     }
     log(JSON.stringify(graph))
-  })
+  }
+
+  // add an arrow
+  if (graph.directed) {
+    console.log(`id1 = ${id1}, id2 = ${id2}`)
+    const arrowEl = document.createElement("span")
+    arrowEl.classList.add("arrow")
+    arrowEl.innerText = '>'
+    arrowEl.style.right = `${nodeRadius}px`
+    arrowEl.style.top = `-${arrowHalfHeight}px`
+    arrowEl.addEventListener("dblclick", () => removeLine(lineEl))
+    lineEl.appendChild(arrowEl)
+  }
+  if (graph.weighted) {
+    const weightInputEl = document.createElement("textarea")
+    weightInputEl.classList.add("weight")
+    weightInputEl.style.minHeight = `0.9rem`
+    weightInputEl.id = `${lineEl.id}-w`
+    weightInputEl.rows = 1
+    weightInputEl.cols = 2
+    weightInputEl.innerText = '1'
+    console.log(`Angel of the edge is ${angel}`)
+    if (angel > Math.PI / 2) {
+      weightInputEl.style.transform = "rotate(180deg) translateY(-10%)";
+    }
+    weightInputEl.addEventListener("keydown", numberInputHandler)
+    weightInputEl.addEventListener("focusout", (event) => {
+      saveWeight(event.target.value.replaceAll(" ", ''), event.target.id)
+    })
+    lineEl.appendChild(weightInputEl)
+  }
+  lineEl.addEventListener("dblclick", (event) => removeLine(event.target))
   lineEl.classList.add("edge")
-  lineEl.id = `${id1}-${id2}`
   boardEl.appendChild(lineEl)
 }
 
@@ -364,6 +464,7 @@ boardEl.addEventListener("click", (event) => {
       if (graph.nodes.length === 0) {
         disableProperties()
         graph.directed = isDirected
+        graph.weighted = isWeighted
       }
       const nodeEl = drawNode(x, y)
       setTimeout(() => {
